8e2fcbb5bb3dc8b84fdcc1af6155167307478292
Pavel19k <Pavel19k@gmail.com>
2022-09-27 21:56:50 +0300

    pavel varastaa tän nyt

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ const App = () => {
     </>
   );
 };
-// testing lol
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -22,4 +21,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-//home comp
