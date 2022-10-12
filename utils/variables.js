72340869fc7152e497442232cbc698665359c0ec
karoliam <kmultas@gmail.com>
2022-10-12 09:51:01 +0300

    fixed kissalinkki

--- utils/variables.js
+++ utils/variables.js
@@ -1,7 +1,7 @@
 const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
 const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
 const applicationTag = 'korvakarva';
-const kissalinkki = 'http://placekitten.com';
+const kissalinkki = 'http://placekitten.com/100';
 // TODO ? const applicationTag = 'kissakoirajotain';
 const appId = 'buddy#';
 export {apiUrl, mediaUrl, appId, applicationTag, kissalinkki};
