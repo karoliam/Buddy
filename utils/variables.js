113036e0fe947ebf0f70412e1ebc3b39e6c64c21
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2022-10-13 12:34:40 +0300

    moi

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,6 @@
 const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
 const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
-const applicationTag = 'korvakarva';
+const applicationTag = 'korvakarvakorva';
 const kissalinkki = 'https://placekitten.com/100/100';
 // TODO ? const applicationTag = 'kissakoirajotain';
 const appId = 'buddy#';
